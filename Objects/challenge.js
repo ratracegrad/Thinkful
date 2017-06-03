131bf7d392ecc94368231e542bd10ab1dc939a77
Jennifer Bland <ratracegrad@gmail.com>
2017-06-03 09:27:52 -0400

    Objects

---
+++ Objects/challenge.js
@@ -0,0 +1,30 @@
+function getTokens(rawString) {
+  // NB: `.filter(Boolean)` removes any falsy items from an array
+  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
+  
+function mostFrequentWord(text) {
+  var words = getTokens(text);
+  var wordFrequencies = {};
+  for (var i = 0; i <= words.length; i++) {
+    if (words[i] in wordFrequencies) {
+      wordFrequencies[words[i]]++;
+    }
+    else {
+      wordFrequencies[words[i]]=1;
+    }
+  }
+  var currentMaxKey = Object.keys(wordFrequencies)[0];
+  var currentMaxCount = wordFrequencies[currentMaxKey];
+  
+  for (var word in wordFrequencies) {
+    if (wordFrequencies[word] > currentMaxCount) {
+      currentMaxKey = word;
+      currentMaxCount = wordFrequencies[word];
+    }
+  }
+  return currentMaxKey;
+}
+
+var freq = mostFrequentWord('now is the time for all good men to come to the aid of their country');
+console.log(freq);
---
+++ Objects/drill2.js
@@ -0,0 +1,67 @@
+function makeStudentsReport(data) {
+  // your code here
+  var results = [];
+
+  for (var i =0; i< data.length; i++) {
+    var keys = Object.keys(data[i]);
+    var newStr = data[i][keys[0]] + ': ' + data[i][keys[1]];
+    results.push(newStr);
+  }
+
+  return results;
+}
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   Nothing to see here!
+   
+*/
+
+
+// tests
+
+function testIt() {
+
+  var testData = [
+    {name: 'Jane Doe', grade: 'A'},
+    {name: 'John Dough', grade: 'B'},
+    {name: 'Jill Do', grade: 'A'}
+  ];
+
+  var expectations = [
+    'Jane Doe: A',
+    'John Dough: B',
+    'Jill Do: A'
+  ];
+
+  var results = makeStudentsReport(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error(
+      'FAILURE: `makeStudentsReport` must return an array');
+    return
+  }
+  if (results.length !== testData.length) {
+    console.error(
+      'FAILURE: test data had length of ' + testData.length +
+      ' but `makeStudentsReport` returned array of length ' + results.length);
+    return
+  }
+  for (var i=0; i < expectations.length; i++) {
+    var expect = expectations[i];
+    if (!results.find(function(item) {
+      return item === expect;
+    })) {
+      console.error(
+        'FAILURE: `makeStudentsReport` is not ' +
+        'producing expected strings'
+      );
+      return
+    }
+  }
+  console.log('SUCCESS: `makeStudentsReport` is working');
+}
+
+testIt();
