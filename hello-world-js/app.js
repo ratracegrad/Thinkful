9710840b4129c7617e81c1d1dcb9c76d0c045f21
Jennifer Bland <ratracegrad@gmail.com>
2017-06-03 09:29:54 -0400

    challenges

---
+++ hello-world-js/app.js
@@ -0,0 +1,9 @@
+// function main() {
+//   const message = "Hello World from JS!";
+//   console.log(message);
+//   console.log('Hello console');
+//   alert(message);
+// }
+// main();
+
+console.log('hello console');
