83af09570af6d4683d72eadb5e0c1fa4fdbe6080
Jennifer Bland <jenniferbland@Jennifers-MBP.attlocal.net>
2017-06-19 21:33:58 -0400

    jquery code for shopping cart app

--- jquery/shopping-list-app/script.js
+++ jquery/shopping-list-app/script.js
@@ -1,3 +1,24 @@
-/**
- * Created by jenniferbland on 6/19/17.
- */
+$('#js-shopping-list-form').submit(function(event) {
+  event.preventDefault();
+  console.log('submit');
+  const item = $(event.currentTarget).find('#shopping-list-entry');
+  $('.cart').append(`<div class="cartEntry"><h3>${item}</h3><input type="button" value="check" class="checkBtn">
+          <input type="button" value="delete" class="deleteBtn"></div>`);
+  $('#shopping-list-entry').val('');
+  item.val("");
+
+  // const item = $('#shopping-list-entry').val();
+  // $('.cart').append(`<div class="cartEntry"><h3>${item}</h3><input type="button" value="check" class="checkBtn">
+  // <input type="button" value="delete" class="deleteBtn"></div>`);
+  // $('#shopping-list-entry').val('');
+});
+
+
+$('.cart').on('click', '.deleteBtn', function(event) {
+  $(this).parent().remove();
+});
+
+$('.cart').on('click', '.checkBtn', function(event) {
+  $(this).parent().find('h3').toggleClass('check');
+});
+
