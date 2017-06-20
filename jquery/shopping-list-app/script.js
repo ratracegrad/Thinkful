32965dc28f3a5c88af9cba389644295236922802
Jennifer Bland <jenniferbland@Jennifers-MBP.attlocal.net>
2017-06-19 21:56:35 -0400

    updated script for shopping cart app

--- jquery/shopping-list-app/script.js
+++ jquery/shopping-list-app/script.js
@@ -1,16 +1,9 @@
 $('#js-shopping-list-form').submit(function(event) {
   event.preventDefault();
-  console.log('submit');
-  const item = $(event.currentTarget).find('#shopping-list-entry');
-  $('.cart').append(`<div class="cartEntry"><h3>${item}</h3><input type="button" value="check" class="checkBtn">
+  const item = $(event.currentTarget).find('#shopping-list-entry').;
+  $('.cart').append(`<div class="cartEntry"><h3>${item.val()}</h3><input type="button" value="check" class="checkBtn">
           <input type="button" value="delete" class="deleteBtn"></div>`);
-  $('#shopping-list-entry').val('');
-  item.val("");
-
-  // const item = $('#shopping-list-entry').val();
-  // $('.cart').append(`<div class="cartEntry"><h3>${item}</h3><input type="button" value="check" class="checkBtn">
-  // <input type="button" value="delete" class="deleteBtn"></div>`);
-  // $('#shopping-list-entry').val('');
+  item.val('');
 });
 
 
