57e8aee4181b47d9e2cbf8cd2f49799e0815f9e4
Jennifer Bland <jennifer.bland@sbdinc.com>
2017-06-28 16:47:01 -0400

    Updated script

--- jquery/shopping-list-app/script.js
+++ jquery/shopping-list-app/script.js
@@ -1,6 +1,7 @@
 $('#js-shopping-list-form').submit(function(event) {
   event.preventDefault();
-  const item = $(event.currentTarget).find('#shopping-list-entry').;
+  console.log('form');
+  const item = $(event.currentTarget).find('#shopping-list-entry');
   $('.cart').append(`<div class="cartEntry"><h3>${item.val()}</h3><input type="button" value="check" class="checkBtn">
           <input type="button" value="delete" class="deleteBtn"></div>`);
   item.val('');
@@ -14,4 +15,3 @@ $('.cart').on('click', '.deleteBtn', function(event) {
 $('.cart').on('click', '.checkBtn', function(event) {
   $(this).parent().find('h3').toggleClass('check');
 });
-
